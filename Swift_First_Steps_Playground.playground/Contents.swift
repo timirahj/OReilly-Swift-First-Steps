1cdcb30caa33cae33d14c3c46a0d7de4900b2377
Timirah James <timirah.james@gmail.com>
2020-08-26 03:54:59 -0700

    Playground fixes

--- Swift_First_Steps_Playground.playground/Contents.swift
+++ Swift_First_Steps_Playground.playground/Contents.swift
@@ -1,3 +1,13 @@
+
+/*:
+ 
+ # Swift First Steps
+ ### Instructor: Timirah James
+ 
+ ## Let's Dive Into Swift!
+ 
+ */
+
 import UIKit
 
 var str = "Hello, playground"
@@ -14,18 +24,11 @@ print("This should run properly")
 
 
 
-
-
 // Declaring a Constant
 
-let myFirstName:String = "Timirah"
-//myFirstName = "Tinashe"
+let myFirstName: String = "Timirah"
 
 myFirstName
-myFirstName
-
-myFirstName
-
 
 print(myFirstName)
 
@@ -34,17 +37,12 @@ print(myFirstName)
 //Declaring a Variable
 
 var myAgeNow:Int = 23
-
 var yourAgeNow:Int = 60
 
 myAgeNow + yourAgeNow
 
 
 //Syntax -- let/var keyword identifierName:DataType = value
-
-
-
-
 
 
 //Optionals
@@ -55,13 +53,11 @@ print(userName!)
 
 
 if userName != nil {
-    print(userName! + " is a member")
+  print(userName! + " is a member")
 }
-  else {
-    print("Guest user")
+else {
+  print("Guest user")
 }
-
-
 
 
 //Implicitly Unwrapping
@@ -76,14 +72,9 @@ var groceryList: [String] = ["eggs", "milk"]
 print(groceryList)
 
 
-
 groceryList.append("juice")
-
 groceryList.insert("bread", at: 2)
-
 groceryList.count
-
-
 
 
 
@@ -91,12 +82,12 @@ groceryList.count
 //Functions
 
 func nameSports() {
-    
-    let sport1 = "Basketball"
-    let sport2 = "Baseball"
-    let sport3 = "Golf"
-    
-    print("Signing up for \(sport1), \(sport2), and \(sport3) this year.")
+  
+  let sport1 = "Basketball"
+  let sport2 = "Baseball"
+  let sport3 = "Golf"
+  
+  print("Signing up for \(sport1), \(sport2), and \(sport3) this year.")
 }
 
 nameSports()
@@ -106,8 +97,8 @@ nameSports()
 //Functions with Parameters and Return Values
 
 func createUser(name:String, age: Int){
-
-    print("New user: \(name) vs. \(age)")
+  
+  print("New user: \(name) vs. \(age)")
 }
 
 
@@ -116,26 +107,45 @@ createUser(name: "Keisha", age: 11)
 
 func getSum(firstNumber: Int, secondNumber: Int) -> Int{
   
-    //local parameters only
-    return firstNumber + secondNumber
-    
+  //local parameters only
+  return firstNumber + secondNumber
+  
 }
 
 
 getSum(firstNumber: 25, secondNumber: 100)
 var x = getSum(firstNumber: 10, secondNumber: 80)
 
-
 x
 
 
-// Guard let
+
+// if let
+
+func greetStudent() {
+  if let studentName = username {
+    print(studentName + " is a member")
+  }
+}
 
 
-//For and While Loops
-
-
-
-//Dictionaries
-
-
+//  guard let
+ 
+/*
+ func greetStudent() {
+  guard let studentName = username else {
+    return
+  }
+ */
+  
+  
+  
+  //Dictionaries (key-value pairings)
+  var students = ["Name" : "Value"]
+  
+  var capitols = ["California" : "Los Angeles", "Georgia" : "Atlanta", "Florida" : "Miami", "Texas" : "Austin"]
+  
+  capitols["California"]
+  
+  capitols["California"] = "LA"
+// Encodable / Decodable
